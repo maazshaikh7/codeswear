b3ca77dbeb8389e43ae2ae12daf461e1aab37029
Maaz <maazshaikh4559@gmail.com>
2023-07-20 14:55:34 +0530

    Created update product API

---
+++ app/api/updateproducts/route.ts
@@ -0,0 +1,41 @@
+import connectDb from "@/app/middleware";
+import Product from "@/app/models/Product";
+import { NextResponse } from "next/server";
+
+export async function PUT(req: Request) {
+  try {
+    await connectDb();
+    const data = await req.json();
+    const { title, _id, description, slug, quantity, price, categoryID } = data;
+
+    const saveData = await Product.findByIdAndUpdate(
+      { _id },
+      {
+        title: title,
+        description: description,
+        slug: slug,
+        price: price,
+        quantity: quantity,
+        category: categoryID,
+      }
+    );
+
+    if (saveData) {
+      return NextResponse.json({
+        success: true,
+        message: "Product updated successfully!",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to update the product. Please try again!",
+      });
+    }
+  } catch (error) {
+    console.log("Error in updating a new product:", error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong. Please try again!",
+    });
+  }
+}

90cb051a319be6fb30b9f47775d38427cc4363a5
Maaz <maazshaikh4559@gmail.com>
2023-07-20 07:23:17 +0530

    Addproduct API connected to database

---
+++ app/api/addproducts/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import Product from "@/app/models/Product";
+import connectDb from "@/app/middleware";
+import { NextApiRequest } from "next";
+
+export async function POST(req: Request) {
+  try {
+    await connectDb();
+
+    const data = await req.json();
+
+    const {
+      productCategory,
+      productDescription,
+      productFeatured,
+      productImage,
+      productName,
+      productPrice,
+      productQuantity,
+      productSlug,
+    } = data;
+
+    const saveData = await Product.create(data);
+
+    if (saveData) {
+      return NextResponse.json({
+        success: true,
+        message: "Product added successfully!",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to add the Product. Please try again!",
+      });
+    }
+  } catch (error) {
+    console.log("Error in adding a new Product:", error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong. Please try again!",
+    });
+  }
+}

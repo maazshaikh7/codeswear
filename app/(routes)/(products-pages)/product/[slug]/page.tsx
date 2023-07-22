b663c9d46f8b8f42ddf91a37e2b367e5a63b9e78
Maaz <maazshaikh4559@gmail.com>
2023-07-23 02:50:26 +0530

    Populated product/[slug] page with fetched data

--- app/(routes)/(products-pages)/product/[slug]/page.tsx
+++ app/(routes)/(products-pages)/product/[slug]/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CartContext, { CartContextProps } from "@/app/_context/CartContext";
 import { useRouter } from "next/navigation";
 
@@ -11,12 +11,37 @@ interface PageProps {
     slug: string;
   };
 }
+type VariantData = {
+  size: string;
+  color: string;
+  price: number;
+  qtyInStock: number;
+  _id: string;
+};
+
+type ProductData = {
+  _id: string;
+  slug: string;
+  title: string;
+  img: string;
+  description: string;
+  category: string;
+  variants: VariantData[];
+};
 
 const Page: React.FC<PageProps> = ({ params }) => {
   const router = useRouter();
   const { addToCart } = useContext(CartContext) as CartContextProps;
   const [pin, setPin] = useState<null | string>();
   const [service, setService] = useState<null | Boolean>(null);
+  const [productData, setProductData] = useState<ProductData[]>([]);
+  const [lastSelectedColor, setLastSelectedColor] = useState("");
+
+  const handleButtonClick = (color: string) => {
+    addToCart("12345", 1, 500, "T-SHIRT", "S", color);
+    setLastSelectedColor(color);
+  };
+
   const checkServiceAvailability = async () => {
     try {
       const pinsResponse = await fetch("http://localhost:3000/api/pincode");
@@ -32,6 +57,18 @@ const Page: React.FC<PageProps> = ({ params }) => {
       console.error("Error occurred while fetching pin codes:", error);
     }
   };
+
+  useEffect(() => {
+    // Fetch products data from the API
+    fetch("http://localhost:3000/api/getproducts")
+      .then((response) => response.json())
+      .then((data) => setProductData(data))
+      .catch((error) => console.error("Error fetching products data:", error));
+  }, []);
+
+  // Filter the product data based on the provided category
+  const filteredData = productData.filter((item) => item.slug === params.slug);
+  console.log("Filtered data", filteredData);
 
   const addToCartHandler = () => {
     addToCart("12345", 1, 500, "T-SHIRT", "S", "RED");
@@ -54,10 +91,12 @@ const Page: React.FC<PageProps> = ({ params }) => {
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                BRAND NAME
+                {filteredData.map((item: ProductData) =>
+                  item.category.toUpperCase()
+                )}
               </h2>
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                The Catcher in the Rye
+                {filteredData.map((item: ProductData) => item.title)}
               </h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
@@ -158,28 +197,50 @@ const Page: React.FC<PageProps> = ({ params }) => {
                 </span>
               </div>
               <p className="leading-relaxed">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean shorts keytar banjo tattooed umami
-                cardigan.
+                {filteredData.map((item: ProductData) => item.description)}
               </p>
               <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                 <div className="flex">
                   <span className="mr-3">Color</span>
-                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                  {filteredData.map((item: ProductData) =>
+                    item.variants.map(
+                      (variant: VariantData) =>
+                        variant.color && (
+                          <button
+                            key={variant._id}
+                            style={{
+                              backgroundColor: variant.color,
+                              border: "2px solid #D1D5DB",
+                              marginLeft: "0.25rem",
+                              borderRadius: "9999px",
+                              width: "1.5rem",
+                              height: "1.5rem",
+                              outline: "none",
+                              boxShadow:
+                                variant.color === lastSelectedColor
+                                  ? "0 0 0 2px gray"
+                                  : "none",
+                            }}
+                            onClick={() => handleButtonClick(variant.color)}
+                          ></button>
+                        )
+                    )
+                  )}
                 </div>
                 <div className="flex ml-6 items-center">
                   <span className="mr-3">Size</span>
                   <div className="relative">
                     <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
+                      {filteredData.map((item: ProductData) =>
+                        item.variants
+                          .filter(
+                            (variant: VariantData) =>
+                              variant.color === lastSelectedColor // Filter the sizes based on the selected color
+                          )
+                          .map((variant: VariantData) => (
+                            <option key={variant._id}>{variant.size}</option>
+                          ))
+                      )}
                     </select>
                     <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                       <svg
@@ -199,7 +260,10 @@ const Page: React.FC<PageProps> = ({ params }) => {
               </div>
               <div className="flex flex-col space-y-2">
                 <span className="title-font font-medium text-2xl text-gray-900 mb-10">
-                  ₹58.00
+                  ₹
+                  {filteredData.map(
+                    (item: ProductData) => item.variants[0].price
+                  )}
                   <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                     <svg
                       fill="currentColor"

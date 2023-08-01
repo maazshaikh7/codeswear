8c99f32bd5cb3cebe0070ee542ab1a6aa5336794
maazshaikh7 <maazshaikh4559@gmail.com>
2023-08-01 15:12:54 +0530

    changed for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Content from "./_components/Content";
 import Hero from "./_components/Hero";
 export default function Home() {
   return (
-    <main className="w-full text-center">
-      <Hero />
-      <Content />
-    </main>
+    <div>hello</div>
+    // <main className="w-full text-center">
+    //   <Hero />
+    //   <Content />
+    // </main>
   );
 }
